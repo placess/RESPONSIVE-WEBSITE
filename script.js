c68f7960d9ffd4ef46eaeacb4c83c41231d80553
Gabriel Place <143138887+placess@users.noreply.github.com>
2023-12-17 00:48:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(' .menu-desktop', { 
+    rotate: { x: 0, y: 30, z: 0 },
+    duration: 1500 });
+
+sr.reveal(' .interface', { 
+    rotate: { x: 0, y: 30, z: 0 },
+    duration: 1500 });
+
+sr.reveal(' .btn-contato', { 
+    rotate: { x: 0, y: 30, z: 0 },
+    duration: 1500 });
+
+sr.reveal(' .txt-topo-site', { 
+    rotate: { x: 0, y: 30, z: 0 },
+    duration: 1500 });
+
+// MENU MOBILE
+//let btnMenu = document.getElementById('btn-menu')
+//let menu = document.getElementById('menu-mobile')
+//let overlay = document.getElementById('overlay-menu')
+
+//btnMenu.addEventListener('click', ()=>{
+    //menu.classList.add('abrir-menu')
+//})
+
+//menu.addEventListener('click', ()=>{
+    //menu.classList.remove('abrir-menu')
+//})
+
+//overlay.addEventListener('click', ()=>{
+    //overlay.classList.remove('abrir-menu')
+//})
